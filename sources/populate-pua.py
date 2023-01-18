caa1c07d8be6d611776b6b1fd27bbfb4c7eafd22
Panogen <max.kussmann@gmail.com>
2023-01-17 17:02:35 -0800

    Reformatted PUA
    Reordered PUA and added more glyphs (headline colon, semicolon) and reformatted some files

--- sources/populate-pua.py
+++ sources/populate-pua.py
@@ -1,5 +1,5 @@
 import fontforge
-# Must be run through FFPython.exe. Using the inbuilt Python interpreter will crash FontForge.
+# !! Must be run through FFPython.exe. Using the inbuilt Python interpreter will crash FontForge.
 
 
 def create_char(font, uni, glyph_name):
@@ -19,12 +19,12 @@ unusedValues = (
     0xe028, 0xe02d, 0xe02e, 0xe030,
     0xe031, 0xe03c, 0xe03d, 0xe03f,
     0xe040, 0xe042, 0xe043, 0xe04b,
-    0xe04c, 0xe086, 0xe087, 0xe088,
-    -1  # added to prevent out-of-bounds error
+    0xe04c, 0xe087, 0xe089, 0xe08a,
+    0xe08b, 0xe08c, -1  # -1 added to prevent out-of-bounds error
 )
 unusedValueInd = 0
 rangeA = (0xe000, 0xe050)   # Ranges do not include the second number.
-rangeB = (0xe080, 0xe09a)
+rangeB = (0xe080, 0xe09e)
 rangeC = (0xe110, 0xe114)
 
 # Section A ("Connected Forms")
@@ -48,7 +48,8 @@ create_char(current_font, 0xe04f, "xy_kashida")
 unicode = rangeB[0]
 names = (
     "xy_fullstop", "xy_comma", "xy_commacn", "xy_exclamdown",
-    "xy_questiondown", "xy_apos.isol", "xy_apos.init", "xy_apos.medi",
+    "xy_questiondown", "xy_colon", "xy_semicolon", "xy_period",
+    "xy_apos", "xy_apos.isol", "xy_apos.init", "xy_apos.medi",
     "xy_apos.fina", "xy_kome", "xy_lentleft", "xy_lentright",
     "xy_lentwhiteleft", "xy_lentwhiteright", "xy_kagileft", "xy_kagiright",
     "xy_kagiwhiteleft", "xy_kagiwhiteright", "xy_kikkoleft", "xy_kikkoright",
@@ -69,7 +70,7 @@ for name in names:
 if current_font.italicangle != 0:
     create_char(current_font, unicode, "e.fishhook")
 else:
-    print("This is not an italic font. Fishhook E not added")
+    print("Fishhook E not added.")
 
 current_font.save()
 current_font.close()
